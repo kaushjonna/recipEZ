832923eba040cc1e1e906e2c153254c65e790bbf
Kaush <kaush.jonna@gmail.com>
2019-04-02 09:16:32 -0400

    created front end and linked with gql-apollo, added necessary dependancies

--- Frontend/recipEZ/App.js
+++ Frontend/recipEZ/App.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import {createHttpLink } from "apollo-link-http";
+import fetch from 'node-fetch';
+import gql from 'graphql-tag';
+
+
+const client = new ApolloClient({
+  link: createHttpLink({
+    uri: "http://localhost:4000/",
+    fetch: fetch
+  }),
+  cache: new InMemoryCache()
+});
+
+const query = gql`
+    query {
+        recipes {
+          id
+        }
+    }
+`;
+
+client.query({
+  query
+}).catch((error) => {
+  console.log(error);
+  done()
+}).then(console.log);
 
 export default class App extends React.Component {
   render() {
