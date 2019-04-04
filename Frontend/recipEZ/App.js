90bdc243775ace94f0f13a01c5597a253cf9077d
Nik Malhotra <nmal@me.com>
2019-04-04 11:15:12 -0400

    add front page

--- Frontend/recipEZ/App.js
+++ Frontend/recipEZ/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { AppRegistry } from 'react-native';
+import { StyleSheet, AppRegistry, ScrollView, View, Image } from 'react-native';
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
@@ -8,10 +7,9 @@ import fetch from 'node-fetch';
 import gql from 'graphql-tag';
 import styled from 'styled-components/native'
 import { Accelerometer } from 'expo-sensors';
-import { Appbar, Provider as PaperProvider, Text } from 'react-native-paper';
+import { Appbar, Provider as PaperProvider, Text, Title, Paragraph, Button, ActivityIndicator, Card } from 'react-native-paper';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-
-
+import { TabNavigator } from 'react-navigation'
 
 // const client = new ApolloClient({
 //   link: createHttpLink({
@@ -36,15 +34,69 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 // //   done()
 // // }).then(console.log);
 
+
+class TopBanner extends Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: "#6200ee", paddingLeft: 100, paddingTop: 40, paddingBottom: 20 }}>
+        <Image source={require('./assets/appLogoWhite.png')} ></Image>
+      </View>
+    )
+  }
+}
+
+class Loading extends Component {
+  render() {
+    return (
+      <PaperProvider>
+        <ActivityIndicator animating={true} />
+      </PaperProvider>
+    )
+  }
+}
+
+class Intro extends Component {
+  render() {
+    return (
+      <PaperProvider>
+        <Title>
+          Welcome, {this.props.name}!
+        </Title>
+      </PaperProvider>
+    )
+  }
+}
+
+class CallToAction extends Component {
+  render() {
+    return (
+      <PaperProvider style={styles.container}>
+        <Title style={{ textAlign: 'center' }}>Wanna Cook?</Title>
+        <Button style={styles.button} mode="contained" onPress={() => alert('Find Me A Recipe Pressed')}>Find Me a Recipe</Button>
+        <Button style={styles.button} mode="contained" onPress={() => alert('Find Me A Recipe Pressed')}>I Have a Recipe</Button>
+      </PaperProvider>
+    )
+  }
+}
+
+
+
+
 export default class App extends Component {
   render() {
     return (
       <PaperProvider>
+        <TopBanner></TopBanner>
+        <ScrollView>
+          <Intro name="Nik"></Intro>
+          <CallToAction></CallToAction>
+          <Loading></Loading>
+        </ScrollView>
         <Appbar style={styles.bottom}>
           <Appbar.Action icon="home" onPress={() => alert('Pressed Home')} />
           <Appbar.Action icon="person" onPress={() => alert('Pressed Profile')} />
           <Appbar.Action icon="camera" onPress={() => alert('Pressed Camera')} />
-          <Appbar.Action icon="label" onPress={() => alert('Pressed Label')} />
+          <Appbar.Action icon="save" onPress={() => alert('Pressed Saved')} />
           <Appbar.Action icon="settings" onPress={() => alert('Pressed Settings')} />
         </Appbar>
       </PaperProvider>
@@ -59,15 +111,27 @@ AppRegistry.registerComponent('main', () => Main);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff00cc',
     alignItems: 'center',
     justifyContent: 'center',
 
   },
   bottom: {
+    flex: 1,
+    justifyContent: 'space-between',
     position: 'absolute',
     left: 0,
     right: 0,
     bottom: 0,
   },
+  top: {
+    backgroundColor: '#6200ee',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  button: {
+    margin: 10
+  }
+
 });
